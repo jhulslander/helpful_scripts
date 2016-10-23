47be0bc229dba22e40ac6f0415e4a0868d2499f4
Jay Hulslander <jdh34@cornell.edu>
2016-10-23 09:33:34 -0400

    clear service def tables and run local unit tests for cu kfs

---
+++ clean_service_def.sql
@@ -0,0 +1,5 @@
+delete from KRSB_SVC_DEF_T;
+delete from KRSB_SVC_DSCRPTR_T;
+delete from KRSB_MSG_QUE_T;
+delete from KRSB_MSG_PYLD_T;
+rollback;
---
+++ run_unit_tests_cukfs.sh
@@ -0,0 +1,2 @@
+mvn -e -U clean test -Dadditional.kfs.config.locations=/Users/jdh34/kuali/test/dev/kfs-upgrade-unt.properti  -DskipTests=false -DfastLocalTests
+
