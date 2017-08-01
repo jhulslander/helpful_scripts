68965f06bb3be0e3d5cc1823f3cd9e471e1afe87
Jay Hulslander <jdh34@cornell.edu>
2017-08-01 14:50:10 -0400

    unit tests examples

--- run_unit_tests_cukfs.sh
+++ run_unit_tests_cukfs.sh
@@ -1,2 +1,5 @@
 mvn -e -U clean test -Dadditional.kfs.config.locations=/Users/jdh34/kuali/test/dev/kfs-upgrade-unt.properti -DskipTests=false -DfastLocalTests=true
 
+mvn -e -U test -Dadditional.kfs.config.locations=/Users/jdh34/kuali/test/dev/kfs-upgrade-unt.properties -DskipTests=false
+
+mvn -e -U test -Dadditional.kfs.config.locations=/Users/jdh34/kuali/test/dev/kfs-upgrade-unt.properties -DskipTests=false  -Dtest=edu.cornell.kfs.module.purap.service.impl.CuPurapGeneralLedgerServiceImplTest
